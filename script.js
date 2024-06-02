2ee95f3f52566fd706b3319672e3465a543ab0df
richycandeveloper <ugwuugo92@gmail.com>
2024-06-02 10:51:54 -0700

    i add somethin for index page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuIcon.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+});
